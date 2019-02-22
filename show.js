48e7ec770a76feabe667a527421ec92ccc17cb42
Sandy Rech <sandy_rech@ku.edu>
2019-02-22 17:42:39 -0600

    added all whale images and started next function

---
+++ show.js
@@ -0,0 +1,17 @@
+function myNext()
+{
+  let currentstate = document.getElementById("1").value;
+  if (document.getElementById("1").style.display = "")
+  {
+    document.getElementById("1").style.display = "none";
+    document.getElementById("2").style.display = "";
+  }
+
+  //var theImgTag = document.getElementById("myImgId")
+
+}
+function myPrevious()
+{
+  document.getElementById("1").style.display = "";
+  document.getElementById("2").style.display = "none";
+}
