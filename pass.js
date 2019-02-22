5c762ef5b93a757ff4df10fdf9f80958b1b1c2ba
Sandy Rech <sandy_rech@ku.edu>
2019-02-22 16:37:07 -0600

    added java script file for button function

---
+++ pass.js
@@ -0,0 +1,12 @@
+function myVal()
+{
+  var pass1=document.getElementById("password1");
+  var pass2=document.getElementById("password2");
+  if (pass1==pass2)
+  {
+    window.alert("YOU DID IT!");
+  }
+  else {
+    window.alert("YOU DID NOT DO IT!");
+  }
+}
