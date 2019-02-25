cab7b1b8aa348876acb457040a9431b6aa0bb8f5
Sandy Rech <sandy_rech@ku.edu>
2019-02-24 20:38:19 -0600

    began exercise 4 and tried changing border width

---
+++ EX_4/change.js
@@ -0,0 +1,10 @@
+function myChange()
+{
+//  var my_red=document.getElementById("r_bord").value;
+  //var my_green=document.getElementById("g_bord").value;
+  //var my_blue=document.getElementById("b_bord").value;
+  var my_width=document.getElementById("w");
+
+  my_width.style.borderWidth = "thin"
+
+}
