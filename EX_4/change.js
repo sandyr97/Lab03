71f67fc407b2cc98acf04e5ebf9e8b64dbe61bc6
Sandy Rech <sandy_rech@ku.edu>
2019-02-27 14:18:06 -0600

    added changing border thickness

--- EX_4/change.js
+++ EX_4/change.js
@@ -6,6 +6,7 @@ function myChange()
   let bordRed=document.getElementById("r_bord").value;
   let bordGreen=document.getElementById("g_bord").value;
   let bordBlue=document.getElementById("b_bord").value;
+  let bordWidth=document.getElementById("w_bord").value;
   let changed=document.getElementById("m_text");
   if (backRed<0 || backGreen<0 || backBlue<0 || backRed>255 || backGreen>255 || backBlue>255)
   {
@@ -25,5 +26,7 @@ function myChange()
     changed.style.borderColor = 'rgb(' + bordRed + ',' + bordGreen + ',' + bordBlue + ')';
 
   }
+
+  changed.style.borderWidth = bordWidth
 
 }
