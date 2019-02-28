baf7f66ac9a93bf61824f86b5bb1ac84ca921ca0
Sandy Rech <sandy_rech@ku.edu>
2019-02-27 21:00:13 -0600

    fixed boundary to include 8 chars in passwords

--- EX_1/pass.js
+++ EX_1/pass.js
@@ -4,8 +4,8 @@ function myVal()
   var pass2=document.getElementById("password2").value;
   if (pass1!==pass2)
   {
-    if (pass1.length<= 8 || pass2.length<=8){
-    window.alert("Please enter a password with more than eight characters!");
+    if (pass1.length< 8 || pass2.length<8){
+    window.alert("Please enter a password with at least eight characters!");
   }
     else {
       window.alert("The password does not matches!");
@@ -13,8 +13,8 @@ function myVal()
 
   }
   else {
-    if (pass1.length<= 8 || pass2.length<=8)
-    window.alert("Please enter a password with more than eight characters!");
+    if (pass1.length< 8 || pass2.length<8)
+    window.alert("Please enter a password with at least eight characters!");
     else {
       window.alert("The password is a match");
     }
